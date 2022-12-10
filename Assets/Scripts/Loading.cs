ec5944e0762264d7c768c439e8c03efdc58c1130
Imsung_You <isyou7487@naver.com>
2022-12-10 16:25:08 +0900

    로딩 ui 추가

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI lodingText;
+
+    float endAlpha = 0.0f;
+    float startAlpha = 1.0f;
+
+    float animationTime = 1.0f;
+    float deltaTime = 0.0f;
+
+
+    void Update()
+    {
+        if (animationTime < deltaTime)
+        {
+            deltaTime = 0.0f;
+            float tempAlpha = startAlpha;
+            startAlpha = endAlpha;
+            endAlpha = tempAlpha;
+        }
+
+        deltaTime += Time.deltaTime;
+
+        float alpha = Mathf.Lerp(startAlpha, endAlpha, deltaTime / animationTime);
+        lodingText.color = new Color(lodingText.color.r, lodingText.color.g, lodingText.color.b, alpha);
+    }
+}
