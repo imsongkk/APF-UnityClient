acacf6f2cd25a0fefe5084933062df84e87997c2
Imsung_You <isyou7487@naver.com>
2022-12-10 16:15:30 +0900

    - tmp import - 씬 분리 - 폰트 추가 - 게임매니저 생성

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public static GameManager Instance { get { Init(); return instance; } }
+
+    public static void Init()
+    {
+        if (instance == null)
+        {
+            GameObject go = GameObject.Find("GameManager");
+            if (go == null)
+            {
+                go = new GameObject { name = "GameManager" };
+                go.AddComponent<GameManager>();
+            }
+            instance = go.GetComponent<GameManager>();
+
+            DontDestroyOnLoad(instance.gameObject);
+        }
+    }
+}
